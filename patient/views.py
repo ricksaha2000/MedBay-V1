5a2d21503d99d8a3f807bc4bdf35d42443a1ca03
Jayit Saha <jayit.saha2018@vitstudent.ac.in>
2021-11-07 23:36:43 +0530

    Add booking rapid appointment api

--- patient/views.py
+++ patient/views.py
@@ -6,19 +6,26 @@ from django.shortcuts import render
 from django.views import View
 from doctor.models import Test
 # Create your views here.
-
+import requests
 
 class DashboardView(View):
     template_name = "patient/dashboard.html"
 
     def get(self, request):
         return render(request, self.template_name)
+    def post(self, request):
+        content='Hi '+request.user.username+'! '+'Your recent booking with booking id: B2X1H3'+' has been confirmed on 14 Nov 2021 with Dr. Ruby Perrin - Dental. Stay Safe! ~Team MedBay'
+        r = requests.get(url = f"https://rapidapi.rmlconnect.net:9443/bulksms/bulksms?username=rapid-l9xh6359810000&password=617bf2eb245383001100f8a6&type=0&dlr=1&destination=917044659720&source=RMLPRD&message={content}")
+        print("Status Code", r.status_code)
+        print("ZWEEEEEEEEEEEEE")
+
+        return render(request, self.template_name)
 
 
 @csrf_exempt
 def heart_beat(request):
     inp = request.POST.get('param')
-    out = run([sys.executable, 'C:\\Users\\jayit\\Downloads\\medbay\\HeartRateMonitorGui\\GUI.py'],
+    out = run([sys.executable, 'C:\\Users\jayit\\Downloads\\RAPID\\MedBay-V1\\HeartRateMonitorGui\\GUI.py'],
               shell=True, stdout=PIPE)
     print(out)
     return render(request, 'patient/dashboard.html', {'data1': out.stdout})
